8e8a086813d2e6cc65eafbae4e385a1256153055
pedroarthurSFTW <pedroarthur0017@hotmail.com>
2025-04-12 16:12:09 -0300

    add screen text teste

--- app/src/main/java/com/github/byteguessapplication/presentation/screens/CardListScreen.kt
+++ app/src/main/java/com/github/byteguessapplication/presentation/screens/CardListScreen.kt
@@ -1,9 +1,28 @@
 package com.github.byteguessapplication.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import com.github.byteguessapplication.presentation.viewmodel.CardViewModel
 
 @Composable
 fun CardListScreen(viewModel: CardViewModel) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            "Quem ler eh gay",
+            color = Color.Red, // Cor contrastante
+            fontSize = 30.sp
+        )
+    }
     //TODO:implementação da tela
 }
